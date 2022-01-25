8a0ff357530d771922e3667e4190ee828e0de5cd
Maria-MAM <mariamihaila1409@gmail.com>
2022-01-25 13:19:38 +0200

    implement basic auth

---
+++ src/main/java/com/movieDB/movieDB/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.movieDB.movieDB.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class AuthController {
+
+    private Map<String, LocalDateTime> usersLastAccess = new HashMap<>();
+
+    @GetMapping("/")
+    public String getCurrentUser(@AuthenticationPrincipal User user, Model model) {
+        String username = user.getUsername();
+
+        model.addAttribute("username", username);
+        model.addAttribute("lastAccess", usersLastAccess.get(username));
+
+        usersLastAccess.put(username, LocalDateTime.now());
+
+        return "home";
+    }
+}
